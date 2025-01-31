fa50a8aab331aacd2614547d6c55e694bd404a7a
Muhammad Rehan <rehany7xa@gmail.com>
2025-01-31 17:22:28 +0500

    created the frontend backend and dashboard

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
+});
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
